e5f12af59d50283ed2cc771c50fd4f63c072508b
Karol Wadolowski <karolw155@gmail.com>
2022-12-10 18:00:13 -0500

    Adding some functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,41 @@
+use std::fmt::{Debug, Display, UpperHex};
 use std::ops::{Add, AddAssign,
                Sub, SubAssign,
                Mul, MulAssign,
                Div, DivAssign,
                BitXor, BitXorAssign,
-               BitAnd};
+               Shr};
 
-trait GFStorageType {}
+pub mod gf2;
 
-struct GF<T: GFStorageType, const POLY: u128> {
-    value: T,
-}
+pub trait GFStorageType: Clone + Copy + Sized + Shr + BitXor + BitXorAssign + Eq + PartialEq + Debug + Display + UpperHex {}
+impl GFStorageType for u8 {}
+impl GFStorageType for u16 {}
+impl GFStorageType for u32 {}
+impl GFStorageType for u64 {}
+impl GFStorageType for u128 {}
 
-impl<T: GFStorageType + BitXor<Output = T>, const POLY: u128> Add<GF<T, POLY>> for GF<T, POLY> {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            value: self.value ^ other.value,
-        }
-    }
-}
-
-trait GFType {}
-
-impl<T: GFStorageType, const POLY: u128> GFType for GF<T, POLY> {}
-
-trait GaloisField<T: GFType> {
+pub trait GaloisField<T: Eq + PartialEq + Clone + Copy +
+                         Add + AddAssign + Sub + SubAssign +
+                         Mul + MulAssign> {
     type Elem;
     fn zero() -> Self::Elem;
     fn one() -> Self::Elem;
 }
 
-macro_rules! implement_traits_for_types {
-    ($($type:ty,)*) => {
-    $(
-        impl GFStorageType for $type {}
-
-        impl<const POLY: u128> GaloisField<GF<$type, POLY>> for GF<$type, POLY> {
-            type Elem = GF<$type, POLY>;
-            fn zero() -> Self::Elem { Self::Elem { value: 0 } }
-            fn one() -> Self::Elem { Self::Elem { value: 1 } }
-        }
-    )*
-    }
-}
-
-implement_traits_for_types! {
-    u8,
-    u16,
-    u32,
-    u64,
-    u128,
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::gf2;
 
     #[test]
     fn compile_test() {
-        let gf0 = GF::<u32, 0x7>{value: 1};
-        let gf0 = GF::<u32, 7>::zero();
-        let gf1 = GF::<u32, 0x7>{value: 2};
+        type GF = gf2::GF<u8, 0x7>;
+        //let gf0 = GF{value: 1};
+        let gf0 = GF::zero();
+        let mut gf1 = GF{value: 2};
+        gf1 += gf1;
         let gf2 = gf0 + gf1;
-        assert_eq!(gf2.value, 2);
+        assert_eq!(gf2, gf1);
     }
 }

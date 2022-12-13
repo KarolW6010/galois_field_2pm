4ba158e045d7d2d4c5a548ac487e64fbc9068efd
Karol Wadolowski <karolw155@gmail.com>
2022-12-12 22:43:03 -0500

    Working GF with u8 LUT

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,9 @@
-use std::fmt::{Debug, Display, UpperHex};
-use std::ops::{Add, AddAssign,
-               Sub, SubAssign,
-               Mul, MulAssign,
-               Div, DivAssign,
-               BitXor, BitXorAssign,
-               Shr};
+use std::fmt::{Debug, Display};
+use std::ops::{Add, Sub, Mul, Div, AddAssign, SubAssign, MulAssign, DivAssign};
 
 pub mod gf2;
+pub mod gf2_lut;
 
-pub trait GFStorageType: Clone + Copy + Sized + Shr + BitXor + BitXorAssign + Eq + PartialEq + Debug + Display + UpperHex {}
-impl GFStorageType for u8 {}
-impl GFStorageType for u16 {}
-impl GFStorageType for u32 {}
-impl GFStorageType for u64 {}
-impl GFStorageType for u128 {}
+pub trait GaloisField: Clone + Copy + Eq + PartialEq + Debug + Display +
+                       Add + Sub + Mul + Div + AddAssign + SubAssign + MulAssign + DivAssign {}
 
-pub trait GaloisField<T: Eq + PartialEq + Clone + Copy +
-                         Add + AddAssign + Sub + SubAssign +
-                         Mul + MulAssign> {
-    type Elem;
-    fn zero() -> Self::Elem;
-    fn one() -> Self::Elem;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::gf2;
-
-    #[test]
-    fn compile_test() {
-        type GF = gf2::GF<u8, 0x7>;
-        //let gf0 = GF{value: 1};
-        let gf0 = GF::zero();
-        let mut gf1 = GF{value: 2};
-        gf1 += gf1;
-        let gf2 = gf0 + gf1;
-        assert_eq!(gf2, gf1);
-    }
-}

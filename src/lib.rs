1a82d86c6b9069b9a900418f0e6dc82c53de4946
Karol Wadolowski <karolw155@gmail.com>
2022-12-13 21:43:06 -0500

    Check for primitive polynomial in validation and add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use std::fmt::{Debug, Display};
 use std::ops::{Add, Sub, Mul, Div, AddAssign, SubAssign, MulAssign, DivAssign};
 
-pub mod gf2;
 pub mod gf2_lut;
 
 pub trait GaloisField: Clone + Copy + Eq + PartialEq + Debug + Display +
